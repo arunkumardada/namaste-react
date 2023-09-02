d1ac7417b4edf3719a90e5de3f389fbea961b5e8
Arun Kumar <cdarun.chinna@gmail.com>
2023-09-02 12:51:39 -0400

    episode - 06 part 04
    Shimmer UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,28 @@
 import RestaurantCard from "./RestaurantCard";
-import { resList } from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ShimmerComponent from "./Shimmer";
 const Body = () => {
   //Local State Variable - Super Powerful Variable
-  const [listofRestaurants, setlistofRestaurants] = useState(resList);
+  const [listofRestaurants, setlistofRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.385044&lng=78.486671&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    //Optional Chaining
+    setlistofRestaurants(
+      json?.data.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  if (listofRestaurants.length === 0) {
+    return <ShimmerComponent />;
+  }
   return (
     <div className="body">
       <div className="filter">
