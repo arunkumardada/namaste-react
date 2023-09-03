d873648435ac5a692806545673d398d63c513617
Arun Kumar <cdarun.chinna@gmail.com>
2023-09-03 11:17:53 -0400

    episode 08 part 02
    create state variables in class components

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,10 @@
+import { useState } from "react";
+
 const User = () => {
+  const [count] = useState(0);
   return (
-    <div>
+    <div className="user-card">
+      <h2>Count: {count}</h2>
       <h1>Arun Kumar Dada</h1>
       <h2>Contact</h2>
       <h3>Email ID</h3>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,11 +4,19 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
 
-    //console.log(props)
+    //console.log(props);
+
+    this.state = {
+      count: 0,
+      count2: 2,
+    };
   }
   render() {
     return (
-      <div>
+      <div className="user-card">
+        <h2>Count: {this.state.count}</h2>
+        <h2>Count2: {this.state.count2}</h2>
+
         <h1>{this.props.name}</h1>
         <h2>Contact</h2>
         <h3>Email ID</h3>
